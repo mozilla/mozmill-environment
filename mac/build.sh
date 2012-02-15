452a46db60a74b11e188e49e4fb2406c4e5b1f18
Henrik Skupin <mail@hskupin.info>
2012-02-15 11:40:58 +0100

    Update virtualenv to v1.7. issue #15

--- mac/build.sh
+++ mac/build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-MOZMILL_VERSION=$1
+VERSION_MOZMILL=$1
 
-MERCURIAL_VERSION=2.1
-PYTHON_VERSION=$(python -c "import sys;print sys.version[:3]")
-VIRTUALENV_VERSION=1.5.2
+VERSION_MERCURIAL=2.1
+VERSION_PYTHON=$(python -c "import sys;print sys.version[:3]")
+VERSION_VIRTUALENV=1.7
 
 ENV_DIR=mozmill-env
-TARGET_ARCHIVE=$(dirname $(pwd))/$MOZMILL_VERSION-$(basename $(pwd)).zip
+TARGET_ARCHIVE=$(dirname $(pwd))/$VERSION_MOZMILL-$(basename $(pwd)).zip
 
-VIRTUALENV_URL=https://bitbucket.org/ianb/virtualenv/raw/$VIRTUALENV_VERSION/virtualenv.py
+URL_VIRTUALENV=https://raw.github.com/pypa/virtualenv/$VERSION_VIRTUALENV/virtualenv.py
 
 
 cleanup () {
@@ -22,9 +22,9 @@ if [ ! -n "$1" ] ; then
   exit 1
 fi
 
-echo "Fetching latest version of virtualenv and creating new environment"
-mkdir tmp && curl $VIRTUALENV_URL > tmp/virtualenv.py
-python tmp/virtualenv.py --no-site-packages $ENV_DIR
+echo "Fetching version $VERSION_VIRTUALENV of virtualenv and creating new environment"
+mkdir tmp && curl $URL_VIRTUALENV > tmp/virtualenv.py
+python tmp/virtualenv.py $ENV_DIR
 
 echo "Activating the new environment"
 source $ENV_DIR/bin/activate
@@ -35,17 +35,17 @@ if [ ! -n "${VIRTUAL_ENV:+1}" ]; then
 fi
 
 echo "Installing required Python modules"
-pip install --upgrade --global-option="--pure" mercurial==$MERCURIAL_VERSION
+pip install --upgrade --global-option="--pure" mercurial==$VERSION_MERCURIAL
 pip install --upgrade simplejson
 
-echo "Installing Mozmill $MOZMILL_VERSION"
-pip install --upgrade mozmill==$MOZMILL_VERSION
+echo "Installing Mozmill $VERSION_MOZMILL"
+pip install --upgrade mozmill==$VERSION_MOZMILL
 
 echo "Deactivating the environment"
 deactivate
 
 echo "Copying template files and reorganizing folder structure"
-mv $ENV_DIR/lib/python$PYTHON_VERSION/site-packages/ $ENV_DIR/python-lib
+mv $ENV_DIR/lib/python$VERSION_PYTHON/site-packages/ $ENV_DIR/python-lib
 
 FOLDERS="lib/ .Python include/ python-lib/pip* bin/activate* bin/python* bin/easy_install* bin/pip*"
 for folder in $FOLDERS ; do
